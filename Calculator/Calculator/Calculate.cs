5d7caeb0b981e80135beec0e7d873ed5e36030d1
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 20:41:21 +0530

    Added:using Abstract class

---
+++ Calculator/Calculator/Calculate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+   abstract class Calculate
+    {
+        public abstract void Multiplication(double ValueOne,double ValueTwo);
+        public abstract void Division(double ValueOne, double ValueTwo);
+    }
+    class Result : Calculate 
+    {
+        public override void Multiplication(double ValueOne, double ValueTwo)
+        {
+            Console.WriteLine("Multiplication result is"+ ValueOne * ValueTwo);
+        }
+        public override void Division(double ValueOne, double ValueTwo)
+        {
+            Console.WriteLine("Division result is" + ValueOne / ValueTwo);
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem Based on Abstract class");
+            Result result = new Result();
+            result.Multiplication(10, 20);
+            result.Division(50, 5);
+        }
+    }
+}
